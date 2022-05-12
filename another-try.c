3b50e4bc50483909196a2ea4a895625cfabb9d09
Daniel <danshmw@outlook.com>
2022-05-12 23:15:39 +0300

    created two files one in c
    another is exe file

---
+++ another-try.c
@@ -0,0 +1,11 @@
+//Program to output the sum of two numbers
+#include<stdio.h>
+
+int main()
+{
+    int a, b, sum;
+    printf("enter the two numbers: ");
+    scanf("%d%d", &a, &b);
+    printf("the sum of two numbers entered %d and %d equals to %d\n", a, b, sum=a+b);
+    return 0;
+}
