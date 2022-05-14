7b7af3cc4e604c79b3dd5106174f88fed47273ae
Daniel <100281706+Dan-dacong@users.noreply.github.com>
2022-05-15 02:08:59 +0300

    Update another-try.c
    Add difference

--- another-try.c
+++ another-try.c
@@ -1,12 +1,14 @@
-//Program to output the sum of two numbers
+//Program to output the sum and diff of two numbers
 #include<stdio.h>
 
 int main()
 {
-    int a, b, sum;
+    int a, b, sum, diff;
     printf("enter the two numbers: ");
     scanf("%d%d", &a, &b);
     sum=a+b;
+    diff=a-b;
     printf("the sum of two numbers entered %d and %d equals to %d\n", a, b, sum);
+    printf("the difference between two numbers %d and %d isis:", a, b, diff);
     return 0;
 }
